577b01c617797ad7fc82db04daf5afbfe593e6d3
PNNhatTram <nhattrampn782004@gmail.com>
2024-08-13 20:14:35 +0700

    update navbar with menu

--- MockProject_072024_Nhom4_FE/src/pages/bodyguard/layout/navbar/Navbar.jsx
+++ MockProject_072024_Nhom4_FE/src/pages/bodyguard/layout/navbar/Navbar.jsx
@@ -1,18 +1,40 @@
-import './Navbar.css'
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faBell, faUser, faBars} from '@fortawesome/free-solid-svg-icons'; 
 
+import './Navbar.css'
 
 export default function Navbar(){
+    const [isMenuVisible, setIsMenuVisible] = useState(false); 
+    const [colorMenu, setColorMenu] = useState('#FFFFFF');
+    const handleMenuClick = () => {
+        setIsMenuVisible(!isMenuVisible);
+        setColorMenu(isMenuVisible ? '#FFFFFF' : '#FFAC41');
+    };
+
     return(
         <div className="Nav">
             <div className="Logo">
                 <img src='/public/logo.png' alt="logo.png" />
             </div>
             <div className="NavItem">
-                <button id='bellIcon'><FontAwesomeIcon icon={faBell} /></button>
-                <button id='userIcon'><FontAwesomeIcon icon={faUser} /></button>
-                <button id='menuIcon'><FontAwesomeIcon icon={faBars} /></button>
+                <div className="TopNav">
+                    <button id='bellIcon'><FontAwesomeIcon icon={faBell} /></button>
+                    <button id='userIcon'><FontAwesomeIcon icon={faUser} /></button>
+                    <button id='menuIcon' onClick={handleMenuClick} style={{ backgroundColor: colorMenu }}><FontAwesomeIcon icon={faBars} /></button>
+                </div>
+                <div className="BottomNav">
+                    {isMenuVisible && (
+                    <div className="MenuItem">
+                        <a href="#" className="ItemLink">Profile</a>
+                        <a href="#" className="ItemLink">Training</a>
+                        <a href="#" className="ItemLink">Schedule</a>
+                        <a href="#" className="ItemLink">Requests</a>
+                        <a href="#" className="ItemLink">Rate</a>
+                        <a href="#" className="ItemLink">Time Keeping</a>
+                    </div>
+                    )}
+                </div>
             </div>
         </div>
     )
